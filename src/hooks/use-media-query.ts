0b080b18894eed75b1b8913d6224015a44cdf59f
PROFE MPRESTES <profematiasprestes@gmail.com>
2026-04-17 03:51:40 -0300

    vava

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -2,4 +2,4 @@
 
 // Re-export from use-mobile for backwards compatibility
 // @deprecated Import directly from @/hooks/use-mobile instead
-export { useMediaQuery } from '@/hooks/use-mobile';
+export { useMediaQuery, useIsDesktop } from '@/hooks/use-mobile';
